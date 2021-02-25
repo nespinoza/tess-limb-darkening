a7c7dbfc8582982cd6ffeefa4611e00eaea6360d
nespinoza <nespinoza@stsci.edu>
2021-02-25 00:57:14 -0500

    working fitting code

--- run_fits.py
+++ run_fits.py
@@ -1,5 +1,12 @@
+# Import python libs:
 import numpy as np
+import os
+
+# Import auxiliary libraries:
 import juliet
+import exoctk
+
+# Import libraries for this script:
 import utils
 
 # Exposure time of 2-min cadence data, in days:
@@ -16,9 +23,9 @@ for planet in target_list.keys():
     # target:
     try:
 
-        planet_data, url = utils.get_target_data(planet)
+        planet_data, url = exoctk.utils.get_target_data(planet)
 
-        # Extract useful data; if not floats, ommit target:
+        # Extract useful data:
         tdur = planet_data['transit_duration']
         tdepth = planet_data['transit_depth']
         period = planet_data['orbital_period']
@@ -26,10 +33,12 @@ for planet in target_list.keys():
         t0 = planet_data['transit_time'] + 2400000.5
         t0_err = (planet_data['transit_time_upper'] + planet_data['transit_time_lower'])*0.5
 
+        # If data is not float (e.g., empty values), reject system:
         if (type(tdur) is float) and (type(tdepth) is float) and (type(period) is float) and (type(period_err) is float) and \
            (type(t0) is float) and (type(t0_err) is float):
             has_data = True
         else:
+            print('Something is wrong with ',planet,' data. Skipping.')
             has_data = False
 
         # Now check eccentricity and omega. If no data, set to 0 and 90:
@@ -46,7 +55,7 @@ for planet in target_list.keys():
     if has_data:
 
         # First, load data for each sector:
-        t, f, ferr = juliet.utils.get_all_TESS_data('TIC ' + target_list['WASP-6']['ticid'])
+        t, f, ferr = juliet.utils.get_all_TESS_data('TIC ' + target_list[planet]['ticid'])
 
         # If it has planetary data, go through each sector. First, estimate the transit depth precision 
         # (assuming a box-shaped transit) we would obtain with this 2-min TESS data. If this gives rise 
@@ -65,5 +74,13 @@ for planet in target_list.keys():
             sigma = np.median(ferr[sector])
             sigma_depth = (2. * sigma)/np.sqrt(I)
 
-            # If larger than 5-sigma, perform the fit:
+            # If initial SNR estimate is larger than 5-sigma, perform the fit:
             if tdepth/sigma_depth > 5:
+                print('\t >> Performing fit for '+sector+'; expected depth precision: ',sigma_depth*1e6,' giving SNR:', tdepth/sigma_depth)
+                if not os.path.exists(planet):
+                    os.mkdir(planet)
+                full_path = planet+'/'+sector
+                utils.fit(t[sector], f[sector], ferr[sector], sector, period, period_err, t0, t0_err, ecc, omega, GPmodel = 'ExpMatern', outpath = full_path)
+                utils.fit(t[sector], f[sector], ferr[sector], sector, period, period_err, t0, t0_err, ecc, omega, GPmodel = 'QP', outpath = full_path)
+            else:
+                print('\t WARNING: ',sector, ' DOES NOT look good! Not doing the fit. Expected depth precision: ',sigma_depth*1e6,' giving SNR:', tdepth/sigma_depth)
